29f69f3677b528aa26ce0761e5381d08e8463700
LuXiangrong <359127443@qq.com>
2015-12-09 17:21:55 +0800

    init

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+"use strict";
+jQuery(function($){
+	var swiperMain = new Swiper('.swiper-container-main', {
+        paginationClickable: true,
+        direction: 'vertical',
+        spaceBetween: 50,
+		nextButton:'.swiper-button-next-main'
+    });
+});
